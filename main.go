c37bc85715c4d927192a58c59ee9e00a6361555f
Shawn Smith <shawnpsmith@gmail.com>
2015-01-24 13:35:56 +0900

    start repo cloning handler

--- main.go
+++ main.go
@@ -18,6 +18,17 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func checkHandler(w http.ResponseWriter, r *http.Request) {
 	//repo := r.FormValue("repo")
+	//if !strings.HasPrefix(repo, "https://") {
+	//	repo = "https://" + repo
+	//}
+	//dir := strings.TrimSuffix(repo, ".git")
+	//split := strings.Split(dir, "/")
+	//dir = fmt.Sprintf("repos/%s", split[len(split)-1])
+	//cmd := exec.Command("git", "clone", repo, dir)
+	//if err != nil {
+	//	http.Error(w, err.Error(), 500)
+	//}
+
 	//checks := []check.Check{check.GoFmt{Dir: repo}}
 	//type resp struct {
 	//	Checks []check.Check
