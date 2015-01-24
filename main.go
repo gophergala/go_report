7e00554c9c11103172ee914640815c90d23ef3af
Shawn Smith <shawnpsmith@gmail.com>
2015-01-24 13:02:07 +0900

    add Name() method to interface, rename check.GoFmtCheck to check.GoFmt

--- main.go
+++ main.go
@@ -15,11 +15,23 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
-func cloneHandler(w http.ResponseWriter, r *http.Request) {
+func checkHandler(w http.ResponseWriter, r *http.Request) {
+	//repo := r.FormValue("repo")
+	//checks := []check.Check{check.GoFmt{Dir: repo}}
+	//type resp struct {
+	//	checks []Check `json:"checks"`
+	//}
+	//for _, c := range checks {
+	//	//
+	//}
+	// clone the repo
+	// run the checks
+	// return the json
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/checks", checkHandler)
 	fmt.Println("Running on 127.0.0.1:8080...")
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
