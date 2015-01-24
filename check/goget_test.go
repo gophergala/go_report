4820fa9d9b9f4c71034a8d4a05a959960b069ff7
Francisco Dalla Rosa Soares <dallarosa@gmail.com>
2015-01-24 13:33:39 +0900

    Tests for the basic go get functionality

--- check/goget_test.go
+++ check/goget_test.go
@@ -3,9 +3,17 @@ package check
 import "testing"
 
 func TestGetPackage(t *testing.T) {
+	//working example
 	path := "github.com/nsf/gocode"
 	err := GetPackage(path)
 	if err != nil {
 		t.Error(err)
 	}
+
+	path = "githubcom/nsf/gocode"
+	err = GetPackage(path)
+	if err == nil {
+		t.Error("Should fail for non-existent repo")
+	}
+
 }
