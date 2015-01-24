0cc43851388c1ea6847778ce8e1b42879b1ec6cf
Shawn Smith <shawnpsmith@gmail.com>
2015-01-24 15:42:20 +0900

    start checks handler

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/gophergala/go_report/check"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,28 +22,47 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func checkHandler(w http.ResponseWriter, r *http.Request) {
-	//repo := r.FormValue("repo")
-	//if !strings.HasPrefix(repo, "https://") {
-	//	repo = "https://" + repo
-	//}
-	//dir := strings.TrimSuffix(repo, ".git")
-	//split := strings.Split(dir, "/")
-	//dir = fmt.Sprintf("repos/%s", split[len(split)-1])
-	//cmd := exec.Command("git", "clone", repo, dir)
-	//if err != nil {
-	//	http.Error(w, err.Error(), 500)
-	//}
+	repo := r.FormValue("repo")
+	if !strings.HasPrefix(repo, "https://") {
+		repo = "https://" + repo
+	}
+	dir := strings.TrimSuffix(repo, ".git")
+	split := strings.Split(dir, "/")
+	dir = fmt.Sprintf("repos/%s", split[len(split)-1])
+	cmd := exec.Command("git", "clone", repo, dir)
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	err := cmd.Wait()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
 
-	//checks := []check.Check{check.GoFmt{Dir: repo}}
-	//type resp struct {
-	//	Checks []check.Check
-	//}
-	//for _, c := range checks {
-	//	//
-	//}
-	// clone the repo
-	// run the checks
-	// return the json
+	checks := []check.Check{check.GoFmt{Dir: dir}}
+	type check struct {
+		Name       string  `json:"name"`
+		Percentage float64 `json:"percentage"`
+	}
+	type checksResp struct {
+		checks []check `json:"checks"`
+	}
+
+	resp := checksResp{}
+	for _, c := range checks {
+		p, err := c.Percentage()
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+		}
+		ch := check{c.Name(), p}
+		resp.checks = append(resp.checks, ch)
+	}
+
+	b, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	w.Write(b)
 }
 
 func main() {
