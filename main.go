f7b2810d376ec1cc61978537d5bfe7751059bbf2
Shawn Smith <shawnpsmith@gmail.com>
2015-01-25 00:36:33 +0900

    split clone logic into separate function

--- main.go
+++ main.go
@@ -26,39 +26,56 @@ func assetsHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, r.URL.Path[1:])
 }
 
+func orgRepoNames(url string) (string, string) {
+	dir := strings.TrimSuffix(url, ".git")
+	split := strings.Split(dir, "/")
+	org := split[len(split)-2]
+	repoName := split[len(split)-1]
+
+	return org, repoName
+}
+
+func dirName(url string) string {
+	org, repoName := orgRepoNames(url)
+
+	return fmt.Sprintf("repos/src/github.com/%s/%s", org, repoName)
+}
+
+func clone(url string) error {
+	org, _ := orgRepoNames(url)
+	if err := os.Mkdir(fmt.Sprintf("repos/src/github.com/%s", org), 0755); err != nil && !os.IsExist(err) {
+		return fmt.Errorf("could not create dir: %v", err)
+	}
+	dir := dirName(url)
+	_, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		cmd := exec.Command("git", "clone", url, dir)
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("could not run git clone: %v", err)
+		}
+	} else if err != nil {
+		return fmt.Errorf("could not stat dir: %v", err)
+	} else {
+		cmd := exec.Command("git", "-C", dir, "pull")
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("could not pull repo: %v", err)
+		}
+	}
+
+	return nil
+}
+
 func checkHandler(w http.ResponseWriter, r *http.Request) {
 	repo := r.FormValue("url")
 	if !strings.HasPrefix(repo, "https://github.com/") {
 		repo = "https://github.com/" + repo
 	}
-	dir := strings.TrimSuffix(repo, ".git")
-	split := strings.Split(dir, "/")
-	org := split[len(split)-2]
-	repoName := split[len(split)-1]
-	if err := os.Mkdir(fmt.Sprintf("repos/src/github.com/%s", org), 0755); err != nil && !os.IsExist(err) {
-		http.Error(w, fmt.Sprintf("Could not create dir: %v", err), 500)
+
+	err := clone(repo)
+	if err != nil {
+		log.Println("ERROR: could not clone repo: ", err)
+		http.Error(w, fmt.Sprintf("Could not clone repo: %v", err), 500)
 		return
-	}
-	dir = fmt.Sprintf("repos/src/github.com/%s/%s", org, repoName)
-	_, err := os.Stat(dir)
-	if os.IsNotExist(err) {
-		cmd := exec.Command("git", "clone", repo, dir)
-		if err := cmd.Run(); err != nil {
-			log.Println("ERROR: ", err)
-			http.Error(w, fmt.Sprintf("Could not run git clone: %v", err), 500)
-			return
-		}
-	} else if err != nil {
-		log.Println("ERROR: ", err)
-		http.Error(w, fmt.Sprintf("Could not stat dir: %v", err), 500)
-		return
-	} else {
-		cmd := exec.Command("git", "-C", dir, "pull")
-		if err := cmd.Run(); err != nil {
-			log.Println("ERROR: ", err)
-			http.Error(w, fmt.Sprintf("Could not pull repo: %v", err), 500)
-			return
-		}
 	}
 
 	type score struct {
@@ -71,6 +88,7 @@ func checkHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	resp := checksResp{}
+	dir := dirName(repo)
 	checks := []check.Check{check.GoFmt{Dir: dir},
 		check.GoVet{Dir: dir},
 		check.GoLint{Dir: dir},
@@ -80,7 +98,7 @@ func checkHandler(w http.ResponseWriter, r *http.Request) {
 	for _, c := range checks {
 		p, out, err := c.Percentage()
 		if err != nil {
-			log.Println("ERROR: ", err)
+			log.Println("ERROR: could not calculate percentage:", err)
 			http.Error(w, fmt.Sprintf("Could not run check: %v", err), 500)
 			return
 		}
@@ -90,7 +108,7 @@ func checkHandler(w http.ResponseWriter, r *http.Request) {
 
 	b, err := json.Marshal(resp)
 	if err != nil {
-		log.Println("ERROR: ", err)
+		log.Println("ERROR: could not marshal json:", err)
 		http.Error(w, err.Error(), 500)
 		return
 	}
