2f3b078123212ffb1bf1948d61c1a0cbce88f7d8
Shawn Smith <shawnpsmith@gmail.com>
2015-01-25 20:48:48 +0900

    apostrophe

--- check/gofmt.go
+++ check/gofmt.go
@@ -16,5 +16,5 @@ func (g GoFmt) Percentage() (float64, []FileSummary, error) {
 
 // Description returns the description of gofmt
 func (g GoFmt) Description() string {
-	return `Gofmt formats Go programs. We run <code>gofmt -s</code> on your code, where <code>-s</code> is for the <a href="https://golang.org/cmd/gofmt/#hdr-The_simplify_command">simplify" command</a>`
+	return `Gofmt formats Go programs. We run <code>gofmt -s</code> on your code, where <code>-s</code> is for the <a href="https://golang.org/cmd/gofmt/#hdr-The_simplify_command">"simplify" command</a>`
 }
